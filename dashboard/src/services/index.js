98ecd28f06157e65bedfaf434c689be4285af7f2
Evandro Siqueira <evandro.siqueira@DESKTOP-FHP3EVC>
2021-01-27 08:31:50 -0300

    Alterado o local do vercel.

--- dashboard/src/services/index.js
+++ dashboard/src/services/index.js
@@ -6,7 +6,7 @@ import UsersService from './users'
 import FeedbacksService from './feedbacks'
 
 const API_ENVS = {
-    production: 'https://backend-treinamento-vue3.vercel.app',
+    production: 'backend-treinamento-vue3.siquersis.vercel.app',
     development: '',
     local: 'http://localhost:3000'
 }
