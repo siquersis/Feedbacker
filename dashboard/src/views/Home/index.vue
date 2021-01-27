a8271c77f9429937ad2c472537e1fbb9028800ca
Evandro Siqueira <evandro.siqueira@DESKTOP-FHP3EVC>
2021-01-27 08:22:15 -0300

    first commit

---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <custom-header
+      @create-account="handleAccountCreate()"
+      @login="handleLogin()"
+    />
+    <contact />
+    <div class="flex justify-center py-10 bg-brand-gray">
+      <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+    function handleLogin () {
+      modal.open({
+        component: 'ModalLogin'
+      })
+    }
+    function handleAccountCreate () {
+      modal.open({
+        component: 'ModalAccountCreate'
+      })
+    }
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
