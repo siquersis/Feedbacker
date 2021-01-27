e28a05591fc6a743cf24516b1cb672f43bfb9bf4
Evandro Siqueira <evandro.siqueira@DESKTOP-FHP3EVC>
2021-01-27 09:01:57 -0300

    Configurado o arquivo css.

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -2,7 +2,11 @@ const colors = require('tailwindcss/colors')
 const pallete = require('./pallete')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
